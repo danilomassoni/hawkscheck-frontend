f0e22927a30e82fbf95fbe1f116bbd9c40575837
danilomassoni <massonidanilo@gmail.com>
2025-04-13 10:29:57 -0300

    Start class student

--- src/pages/StudentsPage.jsx
+++ src/pages/StudentsPage.jsx
@@ -1,5 +1,5 @@
 const StudentsPage = () => {
     return <div>Alunos</div>;
   };
-  
+  // This is a functional component that returns a div with the text "Students".
   export default StudentsPage;
